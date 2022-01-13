599fc4b707f8b4f6cacf07ce5f57f25096d89b22
Choubani Amir <amirensit@gmail.com>
2022-01-13 09:35:04 +0100

    Update BaseIT.java

--- src/test/java/com/sureshtech/springboot/tcdemo/it/BaseIT.java
+++ src/test/java/com/sureshtech/springboot/tcdemo/it/BaseIT.java
@@ -1,18 +1,18 @@
 package com.sureshtech.springboot.tcdemo.it;
 
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @Testcontainers
 public abstract class BaseIT {
